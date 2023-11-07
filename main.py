9a079055121fc0ddd092136e672e5fd2e3688584
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2023-11-07 22:54:56 +0800

    added some performance metrics for two  different portfolios

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def simulate_portfolios(start_date,  end_date, num_securities, num_days, initial
 
     allocated_cash = initial_cash/num_securities
     for idx in range(0, num_securities):
-        percentage_changes = np.random.uniform(-0.12, 0.12, num_days).astype(float)
+        percentage_changes = np.random.uniform(-0.03, 0.03, num_days).astype(float)
         df[f"pct_change_{idx}"] = percentage_changes
         df[f"ret_path_{idx}"  ] = df[f"pct_change_{idx}"].cumsum()
         df[f"cash_path_{idx}" ] = (1+df[f"pct_change_{idx}"]).cumprod()*(allocated_cash)
@@ -32,14 +32,16 @@ def simulate_portfolios(start_date,  end_date, num_securities, num_days, initial
 
     col11, col12 = st.columns(2)
     rebalancing_options = {
+        '3D': "3 Day rebalance",
+        '4D': "4 Day rebalance",
+        '5D': "5 Day rebalance",
         'W': "Weekly rebalance",
         'M': "Monthly rebalance",
-        'Q': "Quarterly rebalance"
     }
     with col11:
         rebalancing_frequency = st.selectbox('Rebalancing period:', list(rebalancing_options.keys()), 
                                format_func=lambda option: rebalancing_options[option], 
-                               index=list(rebalancing_options.keys()).index("M"))
+                               index=list(rebalancing_options.keys()).index("3D"))
 
 
     # for simplicity let's do equally weighted allocation
@@ -50,7 +52,6 @@ def simulate_portfolios(start_date,  end_date, num_securities, num_days, initial
     for date, group in df.groupby(pd.Grouper(freq=rebalancing_frequency)):
         group_df = group.copy()
         allocated_cash = current_portfolio_value/num_securities # equally weighted
-        print(f"allocated cash for {date}: {allocated_cash}")
         for idx in range(0, num_securities):
             group_df[f"rebalanced_cash_path_{idx}" ] = (1+group_df[f"pct_change_{idx}"]).cumprod()*(allocated_cash)
         
@@ -67,10 +68,17 @@ def simulate_portfolios(start_date,  end_date, num_securities, num_days, initial
         rebalanced_df = pd.DataFrame(index=rebalanced_dates)
         df.index = pd.to_datetime(df.index)
         rebalanced_df['value'] = rebalanced_portfolio_values
+        rebalanced_df['rebalanced_pct_change'] = rebalanced_df['value'].pct_change()
+        rebalanced_sr = round(qs.stats.sharpe(returns=rebalanced_df['rebalanced_pct_change']),2)
+        max_dd        = round(qs.stats.max_drawdown(rebalanced_df['rebalanced_pct_change']), 2)
+
         fig, ax = plt.subplots()
         ax.plot(rebalanced_df['value'])
         fig.autofmt_xdate()
         st.pyplot(fig)
+
+        st.text(f"Sharpe Ratio : {rebalanced_sr}")
+        st.text(f"Max DD       : {max_dd}")
         pass
     with col22:
         st.markdown("##### Portfolio with no rebalance")
@@ -78,6 +86,12 @@ def simulate_portfolios(start_date,  end_date, num_securities, num_days, initial
         ax.plot(df['raw_portfolio_cash_path'])
         fig.autofmt_xdate()
         st.pyplot(fig)
+
+        df['raw_portfolio_pct_change'] = df['raw_portfolio_cash_path'].pct_change()
+        raw_sr = round(qs.stats.sharpe(returns=df['raw_portfolio_pct_change']),2)
+        max_dd = round(qs.stats.max_drawdown(df['raw_portfolio_pct_change']), 2)
+        st.text(f"Sharpe Ratio : {raw_sr}")
+        st.text(f"Max DD       : {max_dd}")
         pass
 
 
@@ -91,9 +105,9 @@ def main():
     with col01:
         start_date = st.date_input('Start Date', min_value=None, max_value=None, key=None)
     with col02:
-        num_securities = st.number_input('Number of securities', min_value=2, max_value=50, step=1, value=5)
+        num_securities = st.number_input('Number of securities', min_value=2, max_value=50, step=1, value=50)
     with col03:
-        num_days = st.number_input("Days", min_value=120, max_value=1500, step=30, value=360)
+        num_days = st.number_input("Days", min_value=120, max_value=1500, step=30, value=1000)
     with col04:
         initial_cash = st.number_input("Initial cash $", min_value=10000, step=100, value=10000)
 
